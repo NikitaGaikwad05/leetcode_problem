3aa1a90c91d2d53b10aa8f2134b49fa0ef70add7
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2026-07-04 18:42:33 +0530

    Add Brute.java to find largest element in array
    Implemented a brute force solution to find the largest element in an array using sorting.

---
+++ Brute.java
@@ -0,0 +1,15 @@
+//To find the largest element in Array using Brute Force solution 
+import java.util.Arrays;
+class Brute{
+    public static int sortarry(int[] arr){
+        Arrays.sort(arr);
+        return arr[arr.length-1];
+    }
+    public static void main(String args[]){
+        int[] arr1={3,3,6,1};
+        int[] arr2={3,3,0,99,-40};
+        System.out.println("The largest element in arrsy is"+Brute.sortarry(arr1));
+        System.out.println("The largest element in arrsy is"+Brute.sortarry(arr2));
+
+    }
+}
