bf53bf32d67679567ddc79b104c1bf702b4c3395
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2026-07-29 15:05:58 +0530

    Add majorityElement method to find frequent elements
    Implemented a solution to find all elements that appear more than n/3 times in an integer array.

---
+++ 229.java
@@ -0,0 +1,41 @@
+//Majority Element
+//Given an integer array of size n, find all elements that appear more than ⌊n / 3⌋ times.
+//TimeComplexity:O(n)
+//SpaceComplexity: O(1)
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int n=nums.length;
+        int cnt1=0,cnt2=0;
+        int ele1=Integer.MIN_VALUE,ele2=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            if(cnt1==0 && ele2!=nums[i]){
+                cnt1=1;
+                ele1=nums[i];
+            }
+            else if(cnt2==0 && ele1!=nums[i]){
+                cnt2=1;
+                ele2=nums[i];
+            }
+            else if(nums[i]==ele1){
+                cnt1++;
+            }else if(nums[i]==ele2){
+                cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            if(nums[i]==ele1)cnt1++;
+            if(nums[i]==ele2)cnt2++;
+        }
+        int mini=n/3+1;
+        List<Integer>result=new ArrayList<>();
+        if(cnt1>=mini)result.add(ele1);
+        if(cnt2>mini)result.add(ele2);
+        return result;
+    }
+}
